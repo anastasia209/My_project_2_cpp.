871a91c2fd832aaa61ca27014c636295d4241e9c
Анастасия <boskoridzi@gmail.com>
2026-03-11 13:29:45 +0300

    Add files via upload

---
+++ domashnee_zadanie2.cpp
@@ -0,0 +1,19 @@
+﻿
+
+#include <iostream>
+#include <string>
+
+int main()
+
+{
+	setlocale(LC_ALL, "RU");
+
+	std::string text;
+	std::cout << "Введите число: " << std::endl;
+	std::getline(std::cin, text);
+	std::cout << "Ваше число: " << std::endl << text << std::endl;
+
+	return 0;
+}
+
+
